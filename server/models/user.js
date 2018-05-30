8ff0dbf5a8e9d499d96e25e2cb9862068012b2de
andreip <andrei.popescu@infoworld.ro>
2018-05-30 21:34:01 +0300

    Initial commit

---
+++ server/models/user.js
@@ -0,0 +1,116 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _= require('lodash');
+const bcrypt = require('bcryptjs');
+//we are using a Schema to be able to implement custom methods
+var UserSchema = new mongoose.Schema({//defines a schema for a user
+    email:{
+        default: 'cacat',
+        required:true,
+        type: String,
+        trim:true,
+        minlength:1,
+        unique:true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type:String, 
+        require:true,
+        minlength:6
+    },
+    tokens: [{
+        access:{
+            type: String,
+            required:true
+        },
+        token:{
+            type:String,
+            required:true
+        }
+    }] 
+});
+//UserSchema.methods permite depozitarea metodelor
+UserSchema.methods.toJSON = function(){//determines what data to send back.(we want to send back the id and the email after the request)
+    //toJson reprezinta o metoda suprascrisa ce este apelata in momentul in care trimitem mai departe obiectul catre express ca raspuns
+    var user = this;
+  
+    //var userObject = user.toObject();//user.toObject is responsible for taking your mongoose variable 'user' and converting it into a regular object where only the proprieties available in the document exist
+    
+    return _.pick(user, ['_id', 'email']);
+};
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth'
+    var token = jwt.sign({_id: user._id, access}, 'abc123').toString();//toHexString -> passes the string value as apposed to the object id
+  
+    user.tokens = user.tokens.concat([{access, token}]);
+    return user.save().then(() => {
+        return token;
+    });
+
+};
+UserSchema.methods.removeToken = function(token){
+    var user = this;
+    return user.update({
+        $pull:{//pull from the tokens array propriety the token that we are sending through the function
+            tokens:{
+                token:token
+            }
+        }
+    })//iti permite sa stergi inregistrari care corespund unor criterii dintr-un array care 
+}
+UserSchema.statics.findByToken = function(token){
+    var User = this;
+    var decoded;
+    try{
+        decoded = jwt.verify(token,'abc123');
+    }catch(e){
+        return Promise.reject()//prin return nu se mai executa niciodata ce este mai jos si anume User.findOne...
+    }
+    return User.findOne({
+        _id: decoded._id,//puteam folosi quotes si aici dar nu este obligatoriu
+        'tokens.token':token, //to query a nested document we use quotes. Quotes are required when using a dot in the value
+        'tokens.access':'auth'
+    })
+};
+UserSchema.statics.findByCredentials = function(email, password){
+    var User = this;
+    return User.findOne({email}).then((user) => {
+        if(!user){
+            return Promise.reject();
+        }
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err,res) => {
+                if(res){
+                    resolve(user);
+                }else{
+                    reject();
+                }
+            });
+        });
+    });
+}
+var User = mongoose.model('User',UserSchema);
+
+
+
+UserSchema.pre('save', function(next){//folosim functie ca sa putem utiliza this
+    var user = this;
+    if(user.isModified('password')){//daca se modifica parola dorim sa o hash.uim si sa o stocam. Folosim acest lucru deoarece de exemplu putem modifica mailul => se executa codul pre si se hashuieste parola hashuita si nu dorim acest lucru. Asa ca modificam parola doar atunci cand este nevoie(atunci cand se schimba -> update)
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            })
+        })
+    }else{
+        next();
+    }
+})//run code before the save event 
+module.exports = {User};
+//UserSchema.statics -> model method
+//UserSchema.methods -> instance methods 
---
+++ server/server.js
@@ -0,0 +1,194 @@
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+const bcrypt = require('bcryptjs');
+
+var {mongoose} = require('./db/mongoose');
+var Todo = require('./models/todo').Todo;
+var {User} = require('./models/user');
+const port = process.env.PORT || 3000; // -> setat daca este urcat pe heroku 
+//mongodb permite salvarea documetelor de diferite forme in acceasi colectie. Mongoose permite organizarea acestora prin salvarea inregistrarilor respectand o schema
+
+// var newTodo = new Todo({
+//     text: 'Cook dinner'
+// });
+// newTodo.save().then((doc) => {
+//     console.log('Saved todo', doc)
+// }, (e) =>{
+//     console.log('Unable to save todo',e);
+// });//saves to the mongoDB
+// var secondTodo = new Todo({
+//     text: 'second',
+//     completed: false,
+//     completedAt: 1
+// });
+// secondTodo.save().then((doc) => {
+//     console.log('Saved todo:', doc);
+// }, (e) => {
+//     console.log('Unable to save todo',e);
+// })
+// var user = new User({
+//     email:'   gigi@gmail'
+// });
+// user.save().then((doc) => {
+//     console.log('saved user', user);
+// }, (e) => {
+//     console.log('Unable to save user', user)
+// })
+var app = express();//stocheaza aplicatia express
+app.use(bodyParser.json());//our application uses bodyParser middleware -> the return of bodyParser.json() is a function and that is the middleware that we need to give to express -> we can now send json to our express aplication
+//body parser is going to take the json and convert it into an object, ataching it on to this request object
+var authenticate = (req,res,next) => {
+    var token = req.header('x-auth');//gets the header value. We need to pass the key to know which header we get
+    User.findByToken(token).then((user) =>{//takes the token value and return the appropiate user related to that token 
+        if(!user){
+            return Promise.reject();//de asemenea se executa catch.ul de mai de jos
+        }
+       req.user = user;//atasez userul la request ca sa pot sa il trimit din app.get
+       req.token = token; //atasez tokenul la request
+       next();//apelez next ca sa se execute app.get de mai de jos
+    }).catch((e) => {//se leaga de return  Promise.reject()
+        //401 status inseamna "Authentication is required"
+        res.status(401).send();
+        //aici nu mai apelez next deoarece nu doresc sa se execute codul de mai jos
+    });
+}
+app.post('/todos',authenticate, (req, res) => {//url si functia callback
+    console.log(req.body); //body gets store by body-parser
+    var todo = new Todo({
+        text: req.body.text,
+        _creator: req.user._id
+    });
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+app.get('/todos', authenticate, (req, res) => {
+    Todo.find({//returneaza doar documentele care au acelasi id cu 
+        _creator: req.user._id
+    }).then((todos) => {
+        res.send({todos});//le transmitem inapoi sub forma de obiect pentru ca sa putem adauga noi proprietati in cazul in care am fi avut nevoie
+    }, (e)=>{
+        res.status(400).send(e);
+    })
+});
+app.get('/todos/:id', authenticate, (req, res) => {
+    //req.params reprezinda valoarea id.ului din request
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(400).send(); //return incheie executia
+    }
+    Todo.findOne({
+        _id: id,
+        _creator:req.user._id
+    }).then((todo) =>{
+        if(!todo){
+            return res.status(404).send;
+        }
+        res.status(200).send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
+})
+app.delete('/todos/:id',authenticate, (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(400).send;
+    }
+    Todo.findOneAndRemove({
+        _id:id,
+        _creator:req.user._id
+    }).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send(todo);
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+//patch -> update;
+app.patch('/todos/:id',authenticate, (req,res) => {
+    var id = req.params.id;
+    //se face update doar la ce vrem noi -> text si completed
+    var body = _.pick(req.body, ['text', 'completed']);//picks an array of proprieties that user is going to be able to update
+    console.log(body);
+    if(!ObjectID.isValid(id)){
+        return res.status(400).send;
+    }
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+   Todo.findOneAndUpdate({_id:id,_creator:req.user._id}, {$set:body},{new:true}//new returneaza obiectul nou creat
+   ).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});//same as {todo:todo}
+   }).catch((e) => {
+       res.status(400).send();
+   })
+})
+//create users
+app.post('/user', (req, res) => {
+    var body = _.pick(req.body, ['email','password'])
+    var newUser = new User(body);
+    newUser.generateAuthToken().then((token) => {
+        res.header('x-auth', token).send(newUser);//custom header defined as x-auth(key value pair)
+    }).catch((e) => {
+        res.status(400).send(e);;
+   })
+});
+//middleware function that we are going to use on our routes to make them private.The actual route is not going to run until next is called inside the middleware
+// var authenticate = (req,res,next) => {
+//     var token = req.header('x-auth');//gets the header value. We need to pass the key to know which header we get
+//     User.findByToken(token).then((user) =>{//takes the token value and return the appropiate user related to that token 
+//         if(!user){
+//             return Promise.reject();//de asemenea se executa catch.ul de mai de jos
+//         }
+//        req.user = user;//atasez userul la request ca sa pot sa il trimit din app.get
+//        req.token = token; //atasez tokenul la request
+//        next();//apelez next ca sa se execute app.get de mai de jos
+//     }).catch((e) => {//se leaga de return  Promise.reject()
+//         //401 status inseamna "Authentication is required"
+//         res.status(401).send();
+//         //aici nu mai apelez next deoarece nu doresc sa se execute codul de mai jos
+//     });
+// }
+//private route 
+app.get('/users/me', authenticate, (req, res) =>{//uses the middleware from up above
+    res.send(req.user);
+    //req si res de aici sunt aceleasi ca cele definite in middleware
+});
+//LOGIN -> POST /users/login {email, password}
+app.post('/users/login', (req,res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {//daca este o eroare, se leaga de catch.ul de mai jos
+            res.header('x-auth', token).send(user);
+        })
+   }).catch((e) => {
+        res.status(400).send();
+   });
+})
+//logout
+app.delete('/users/me/token',authenticate, (req,res) => {//sterge tokenul
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send;
+    }, () => {
+        res.status(400).send();
+    });
+});
+app.listen(port, () => {//basic server
+    console.log(`Started on port ${port}`);
+});
+//generateAuthToken method is going to be responsible for adding a token on an individual user document, saving that and returning the token  so we can send it back to the user
+//private routes require an x-auth token, we are going to validate that token, find the user asociated with that token 
+//req and res from the middleware are the same as from the app.get. next exists so you can tell express when your middleware function is done and this is usefull because you can have as much middleware as you want registered to a single express app. 
+//On short -> the application will continue to run after you called next

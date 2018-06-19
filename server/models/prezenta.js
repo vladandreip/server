25bba43d37b31aeb1ed8f21805498ccdd5347c66
andreip <andrei.popescu@infoworld.ro>
2018-06-19 13:32:11 +0300

    Add prezenta and curs

---
+++ server/models/prezenta.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+var Prezenta = mongoose.model('Prezenta', {
+    nume: {
+        type:String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    prenume: {
+        type:String,
+        required: true,
+        minlength: 1,
+        trim:true
+        
+    },
+    grupa:{
+        type:String, 
+        required:true,
+        minlength:1,
+        trim:true
+    },
+    _creator:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    }
+});
+module.exports = {Prezenta};
